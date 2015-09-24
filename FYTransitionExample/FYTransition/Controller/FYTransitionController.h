da8dffe0f2e215dfac2cf3274ac81f66a703fa81
Tony <573682532@qq.com>
2015-09-24 11:42:54 +0800

    添加注释,修改readme
    添加注释,修改readme

--- FYTransitionExample/FYTransition/Controller/FYTransitionController.h
+++ FYTransitionExample/FYTransition/Controller/FYTransitionController.h
@@ -17,15 +17,18 @@
  *  imageView在下一个控制器中的frame,用来设置动画结束后,图片最终显示的位置,
  
  *  特别需要注意的几点:
-    1. 此方法在 viewDidLoad 前面调用,所以应在此方法中调用
- 在实现这个方法的时候必须使用代码来设置图片的位置,因为使用xib布局的时候,在调用此方法的时候该imageview控件还未初始化,所以会返回{0,0,0,0},最终导致动画效果失败.
-    2. 如果使用代码布局
+    1. 此方法在 viewDidLoad 前面调用,所以应在此方法中先设置图片的frame,然后在 viewDidLoad 中添加控件的时候直接调用frame来添加相应的子空间
+    2. 在实现这个方法的时候必须使用代码来设置图片的位置,因为使用xib布局的时候,在调用此方法的时候该imageview控件还未初始化,所以会返回{0,0,0,0},最终导致动画效果失败.
  *
  *  @return imageView在下一个控制器中的frame
  */
 - (CGRect)fy_transitionFinalImageViewFrame;
 
 
+/**
+ *  动画完成之后的回调,在此方法中设置图片
+ *
+ */
 - (void)fy_transitionCompleteAnimateImageView:(UIImageView *)imageView;
 
 @end
